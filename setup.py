7f3a8ba037c8d2e5fc64b34a987dc469357cd146
mcroydon <mcroydon@0d184f44-dd58-11dd-a893-8d7ce9fbef62>
2009-01-08 18:39:27 +0000

    Remove import from setup.py.
    git-svn-id: http://queues.googlecode.com/svn/trunk@10 0d184f44-dd58-11dd-a893-8d7ce9fbef62

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-import queues
+
 setup(name='queues',
       version='0.3',
       description='A lowest-common-denominator API for interacting with lightweight queue services.',
